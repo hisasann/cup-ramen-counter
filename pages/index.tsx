3319743490b9a673d1c61c47236868ba701470b8
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-05-23 15:12:43 +0900

    fix: comment out import Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+//import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -60,7 +60,9 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {
+            //<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            }
           </span>
         </a>
       </footer>
